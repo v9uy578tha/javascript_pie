cf6685759c94aa9ad14d636b972f05c9229edc69
Isaac Rajat <109132922+v9uy578tha@users.noreply.github.com>
2024-02-28 17:09:55 +0000

    done

--- 01_basics/try1.js
+++ 01_basics/try1.js
@@ -1,1 +1,3 @@
-console.log("hello")
+console.log((2095+199)/100);  //printing numbers in javascript
+console.log(Math.round(2.3));  //a function for rounding up
+console.log(Math.round((2095+799)*0.1)/100);
---
+++ 01_basics/try2.js
@@ -0,0 +1,4 @@
+function add(a,b){
+    let c=((a+b)*100)/100;
+    console.log(c);
+}
